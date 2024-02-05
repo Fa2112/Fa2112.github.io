919897acf8d662136b021e9b3becb150a6c24aad
Facundo Talavera <Facundo_2112@outlook.com>
2024-02-04 21:43:07 -0300

    nothing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Fa2112.github.io",
+  site: "https://fa2112.github.io",
   integrations: [tailwind()]
 
 
